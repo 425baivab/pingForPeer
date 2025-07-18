420abc723e93ac4ec6107b122995120154ae0acd
unknown <425baivab>
2025-07-18 13:42:22 +0530

    Remove Downloads folder and update .gitignore

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx'; // or ./services/App.jsx if that's where it is
-import { BrowserRouter } from 'react-router-dom'; // ✅ MUST import this
+import App from './App.jsx'; // 
+import { BrowserRouter } from 'react-router-dom'; //
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter> {/* ✅ Fix: Wrap App in BrowserRouter */}
+    <BrowserRouter> 
       <App />
     </BrowserRouter>
   </React.StrictMode>
